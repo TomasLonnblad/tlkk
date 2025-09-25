444861b5ddfd1f2e09990d2244d03aeae0ef2778
TomasLonnblad <tomaslonnblad@gmail.com>
2025-09-25 10:56:32 +0200

    x

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+
+  output: 'export',
+  distDir: 'dist',
+
+
   images: {
     domains: ["i.scdn.co"],
   },
+
 };
 // next.config.js
 
